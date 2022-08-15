8fc73109aead425237d8e8690684927e2e8bd381
RoyOkite <royokite@gmail.com>
2022-08-15 12:57:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+// Arithmetic lab!
 
-// Write your code here
+const num1 = 31;
+const num2 = 2;
+const num3 = 25;
+const num4 = 7;
+
+let multiply = num1 * num2;
+
+let random = parseInt(((Math.random()* 10) + 1), 10);
+
+let mod = num3 % num4;
+
+let max = Math.max(2, 12, 16, 20, 19);
